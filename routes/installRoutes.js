f5360cf895b919d568d3a6a2904e1ac43a60b377
Giovani Ohira <giovaniohira@gmail.com>
2024-08-28 13:14:33 -0300

    use queryInterface and change endpoint to install

--- routes/installRoutes.js
+++ routes/installRoutes.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { User, Event } = require('../models');
+const { User, Event, Subscription } = require('../models');
+const { sequelize } = require('../models');
 
 // Rota para instalar e popular o banco de dados
-router.get('/install', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
+    const queryInterface = sequelize.getQueryInterface();
     await User.sync({ force: true });
     await Event.sync({ force: true });
-    await require('../seeders/20240818120000-demo-users').up();
-    await require('../seeders/20240818120001-demo-events').up();
-    await require('../seeders/20240818120002-create-default-admin').up();
+    await Subscription.sync({ force: true });
+    await require('../seeders/20240818120000-demo-users').up(queryInterface, sequelize);
+    await require('../seeders/20240818120001-demo-events').up(queryInterface, sequelize);
+    await require('../seeders/20240818120002-create-default-admin').up(queryInterface, sequelize);
+    await require('../seeders/20240819-create-subscriptions').up(queryInterface, sequelize);
+    
     res.json({ message: 'Banco de dados instalado e populado com sucesso!' });
   } catch (error) {
+    console.error(error);
     res.status(400).json({ error: 'Erro ao instalar o banco de dados.' });
   }
 });
